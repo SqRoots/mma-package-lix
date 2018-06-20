d0b4c221d916ba2ea14331a5170bf2136951b3c7
sqroots <lixuan159@gmail.com>
2018-06-20 23:56:40 +0800

    modify comment

--- init/init.m
+++ init/init.m
@@ -1,6 +1,6 @@
 (** User Mathematica initialization file **)
 (* ==================== Load my Packages ==================== *)
 (* == GitHub URL:  https://github.com/SqRoots/mma-package-lix = *)
-Quiet@Get@"D:/GitHub/mma-package-lix/package/lixDA/lixDA.wl"
-Quiet@Get@"D:/GitHub/mma-package-lix/package/lixWeb/lixWeb.wl"
+Quiet@Get@"D:/github/mma-package-lix/package/lixDA/lixDA.wl"
+Quiet@Get@"D:/github/mma-package-lix/package/lixWeb/lixWeb.wl"
 (* =============================================== *)
